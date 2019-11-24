6aea75d7f17a0358466be303f1251b2370cc0886
mbickers <mbickers@umich.edu>
2019-11-24 12:25:03 -0500

    Initial Commit

---
+++ Tallybook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tallybook
+//
+//  Created by Max Bickers on 11/24/19.
+//  Copyright © 2019 Max Bickers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
