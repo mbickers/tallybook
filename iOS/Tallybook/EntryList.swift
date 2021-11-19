03b038c7cc87eeae84483cff23ad65a08f0e2039
Max Bickers <mbickers@umich.edu>
2021-11-19 14:32:37 -0500

    Added EntryList

---
+++ iOS/Tallybook/EntryList.swift
@@ -0,0 +1,49 @@
+//
+//  EntryList.swift
+//  Tallybook
+//
+//  Created by Max Bickers on 11/19/21.
+//  Copyright © 2021 Max Bickers. All rights reserved.
+//
+
+import Foundation
+
+struct EntryList {
+  typealias Entry = (date: Date, value: Int)
+  private static let dateFormatter: DateFormatter = {
+    let df = DateFormatter()
+    df.dateFormat = "yyyy-MM-dd"
+    return df
+  }()
+
+  private var entries = [(formattedDate: String, value: Int)]()
+
+  subscript(date: Date) -> Int {
+    get {
+      let formattedDate = EntryList.dateFormatter.string(from: date)
+      let entry = entries.first { $0.formattedDate == formattedDate }
+      return entry?.value ?? 0
+    }
+
+    set(value) {
+      let formattedDate = EntryList.dateFormatter.string(from: date)
+      entries.removeAll { $0.formattedDate == formattedDate }
+      if value != 0 {
+        let newEntry = (formattedDate, value)
+        entries.append(newEntry)
+        entries.sort { $0.formattedDate > $1.formattedDate }
+      }
+    }
+  }
+
+  mutating func removeEntry(onDate date: Date) {
+    self[date] = 0
+  }
+
+  var allEntries: [Entry] {
+    return entries.map { (formattedDate: String, value: Int) in
+      let date = EntryList.dateFormatter.date(from: formattedDate)!
+      return (date, value)
+    }
+  }
+}
