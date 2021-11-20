6ade18c6089400995f58bd6810fd8cbc52353ffd
Max Bickers <mbickers@umich.edu>
2021-11-19 19:23:28 -0500

    Added local disk repository

--- iOS/Tallybook/Repositories/Repository.swift
+++ iOS/Tallybook/Repositories/Repository.swift
@@ -10,7 +10,7 @@ import Combine
 import Foundation
 
 class Injected {
-  static let repository: Repository = TestRepository()
+  static let repository: Repository = LocalRepository()
 }
 
 protocol Repository {
