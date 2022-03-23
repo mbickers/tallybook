dfe43c1b3aa307438d2913fa9d64afe331042b62
Max Bickers <mbickers@umich.edu>
2022-03-23 12:06:32 -0400

    Added firebase

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { getAuth, signInWithEmailAndPassword, User } from "firebase/auth"
+import { Field, Form, Formik } from "formik"
 
-function App() {
-  return <h1>Tallybook</h1>
+console.log(process.env)
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app)
+const auth = getAuth(app)
+
+const Login = () => {
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={({ email, password }) => {
+        signInWithEmailAndPassword(auth, email, password)
+      }}
+    >
+      <Form>
+        <Field type="text" name="email" />
+        <Field type="text" name="password" />
+        <button type="submit" >Submit</button>
+      </Form>
+    </Formik>
+  )
+}
+
+const App = () => {
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setUser(user)
+      console.log(user)
+    })
+
+    return unsubscribe
+  })
+
+  return <>
+    <h1>Tallybook</h1>
+    {user ?
+      <p>{user.email} <button onClick={() => auth.signOut() }>Sign out</button></p> :
+      <Login />}
+    
+  </>
 }
 
 export default App;
