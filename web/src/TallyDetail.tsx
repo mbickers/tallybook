8daac682ce818596872ad1c91828c668efc48f5b
Max Bickers <mbickers@umich.edu>
2022-04-10 21:11:43 -0400

    Added tally detail table

--- web/src/TallyDetail.tsx
+++ web/src/TallyDetail.tsx
@@ -1,6 +1,24 @@
+import { useContext } from "react"
 import { useParams } from "react-router-dom"
+import { TallyServiceContext } from "./TallyServiceProvider"
 
 export const TallyDetail = () => {
   const id = useParams().id
-  return <p>{id}</p>
+  const tallyService = useContext(TallyServiceContext)
+  const tally = tallyService.tallies?.find(tally => tally.id == id)
+
+  if (!tally) {
+      return <p>Unable to view tally {id}</p>
+  }
+
+  return <table>
+      <tbody>
+          {tally.entries.entries.map(entry => (
+              <tr key={entry.formattedDate}>
+                  <td>{entry.formattedDate}</td>
+                  <td>{entry.value}</td>
+              </tr>
+          ))}
+      </tbody>
+  </table>
 }
