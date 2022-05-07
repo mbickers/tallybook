134c0656f549330db233441e407bc03b57f13ef0
Max Bickers <mbickers@umich.edu>
2022-05-07 12:23:49 -0500

    Redo login UI

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,8 +1,8 @@
-import { Button, FormControl, FormLabel, Input, VStack } from "@chakra-ui/react"
+import { Button, Center, Divider, FormControl, FormLabel, Heading, Input, Text, VStack } from "@chakra-ui/react"
 import { FirebaseApp } from "firebase/app"
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import { AuthError, getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import { Formik, Field } from "formik"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { FirebaseContext } from "../providers/FirebaseProvider"
 
@@ -10,43 +10,54 @@ export const Login = () => {
   const navigate = useNavigate()
   const firebase = useContext(FirebaseContext) as FirebaseApp
   const auth = getAuth(firebase) 
+  const [error, setError] = useState('')
 
   return (
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      onSubmit={({ email, password }) => {
-        signInWithEmailAndPassword(auth, email, password)
-        .then(() => navigate("/tallies"))
-      }}
-    >
-      {({ handleSubmit, errors, touched }) => (
-        <form onSubmit={handleSubmit}>
-          <VStack spacing={4} align="flex-start">
-            <FormControl>
-              <FormLabel htmlFor="email">Email Address</FormLabel>
-              <Field
-                as={Input}
-                id="email"
-                name="email"
-                type="email"
-                variant="filled"
-              />
-            </FormControl>
-            <FormControl isInvalid={!!errors.password && touched.password}>
-              <FormLabel htmlFor="password">Password</FormLabel>
-              <Field
-                as={Input}
-                id="password"
-                name="password"
-                type="password"
-                variant="filled"
-              />
-            </FormControl>
-            <Button type="submit" variant="solid" isFullWidth>
-              Login
-            </Button>
-          </VStack>
-        </form>)}
-    </Formik>
+    <Center bg='gray.100' h='100vh'>
+      <VStack bg='white' w='xs' borderRadius='lg' p='1rem' align='begin'>
+        <Heading>Tallybook</Heading>
+        <Divider />
+        <Text color='red'>{error}</Text>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={({ email, password }) => {
+            signInWithEmailAndPassword(auth, email, password)
+              .then(() => navigate("/tallies"))
+              .catch((error: AuthError) => {
+                setError(error.message)
+              })
+          }}
+        >
+          {({ handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <VStack spacing={4} align="begin">
+                <FormControl>
+                  <FormLabel htmlFor="email">Email</FormLabel>
+                  <Field
+                    as={Input}
+                    id="email"
+                    name="email"
+                    type="email"
+                    variant="filled"
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <Field
+                    as={Input}
+                    id="password"
+                    name="password"
+                    type="password"
+                    variant="filled"
+                  />
+                </FormControl>
+                <Button type="submit" colorScheme='green' variant="solid" isFullWidth>
+                  Login
+                </Button>
+              </VStack>
+            </form>)}
+        </Formik>
+      </VStack>
+    </Center>
   )
 }
