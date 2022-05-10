8ab9c1000ae22a8aea0cb865d888143f077fa705
Max Bickers <mbickers@umich.edu>
2022-05-10 17:31:18 -0500

    Linted again

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -1,5 +1,5 @@
 export default function formattedDate(date: Date): string {
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${date.getFullYear()}-${month}-${day}`;
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${date.getFullYear()}-${month}-${day}`;
 }
