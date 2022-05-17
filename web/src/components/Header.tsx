c4e8de8e60479920e7e4c993ef468129901d1a4c
Max Bickers <mbickers@umich.edu>
2022-05-17 15:16:22 -0500

    Refactored header and fixed bug with state

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -3,34 +3,53 @@ import {
   Box, Flex, Spacer, Menu, MenuButton, Button, MenuList, MenuItem,
 } from '@chakra-ui/react';
 import { FirebaseApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { getAuth, User } from 'firebase/auth';
+import { useContext, useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { FirebaseContext } from '../providers/FirebaseProvider';
 import TallybookHeading from './TallybookHeading';
 
-export default function Header() {
+function UserBox() {
   const firebase = useContext(FirebaseContext) as FirebaseApp;
   const auth = getAuth(firebase);
-  const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>(auth.currentUser);
+  const location = useLocation();
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((newUser) => {
+      setUser(newUser);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  if (!user) {
+    return <Button as={Link} size="sm" colorScheme="accent" to="/signin">Sign In</Button>;
+  }
+
+  if (location.pathname.startsWith('/tallies')) {
+    return (
+      <Menu>
+        <MenuButton as={Button} rightIcon={<ChevronDownIcon />} variant="ghost">
+          {user.email}
+        </MenuButton>
+        <MenuList>
+          <MenuItem onClick={() => auth.signOut()}>Sign Out</MenuItem>
+        </MenuList>
+      </Menu>
+    );
+  }
+
+  return <Button as={Link} size="sm" colorScheme="accent" to="/tallies">Go to Tallies</Button>;
+}
+
+export default function Header() {
   return (
     <Box as="header" pos="fixed" w="100%" bg="white" zIndex="1">
       <Flex maxW="50rem" m="auto" alignItems="center">
         <TallybookHeading />
         <Spacer />
-        {auth.currentUser
-          ? (
-            <Menu>
-              <MenuButton as={Button} rightIcon={<ChevronDownIcon />} variant="ghost">
-                {auth.currentUser.email}
-              </MenuButton>
-              <MenuList>
-                <MenuItem onClick={() => auth.signOut()}>Sign Out</MenuItem>
-              </MenuList>
-            </Menu>
-          )
-          : <Button size="sm" colorScheme="accent" onClick={() => navigate('/signin')}>Sign In</Button> }
+        <UserBox />
       </Flex>
     </Box>
   );
