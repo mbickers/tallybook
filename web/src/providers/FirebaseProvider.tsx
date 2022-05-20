17234bc23c57dc0f3b53944e61dcd88206cbf8fc
Max Bickers <mbickers@umich.edu>
2022-05-20 16:29:00 -0500

    Added firebase config

--- web/src/providers/FirebaseProvider.tsx
+++ web/src/providers/FirebaseProvider.tsx
@@ -5,15 +5,7 @@ export const FirebaseContext = React.createContext<FirebaseApp | null>(null);
 
 export function FirebaseProvider({ children }: { children: ReactNode }) {
   const app = useMemo(() => {
-    const firebaseConfig = {
-      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    };
-
+    const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG as string);
     return initializeApp(firebaseConfig);
   }, []);
 
